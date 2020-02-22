d642a6f222b026e930e438c89b2c8cc03d6d3ed4
Nandini Yadav Janga <yadavnandini97@gmail.com>
2020-02-21 20:15:07 -0600

    added implementation

---
+++ sum-of-victims/1mapper.py
@@ -0,0 +1,17 @@
+#This file is to map the columns
+input = open("accidents_2017.csv", "r")
+output = open("01.txt", "w")
+count = 0
+
+for line in input:
+    datalist = line.strip().split(",")
+    id, district,weekday, month, day, hour, partoftheday,mild, serious, victims, vehiclesinvolved = datalist
+   #writing only required columns into the output file 
+    output.write(district + "\t" + victims + "\n")
+
+
+
+ 
+input.close()
+output.close()
+print(output)
---
+++ sum-of-victims/3reducer.py
@@ -0,0 +1,28 @@
+s = open("02.txt","r")
+r = open("03.txt", "w")
+
+thisKey = ""
+thisValue = 0
+
+for line in s:
+  data = line.strip().split('\t')
+  partoftheday, victims = data
+
+  if partoftheday != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = partoftheday 
+    thisValue = 0
+  
+  # apply the aggregation function
+  thisValue += int(victims)
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+s.close()
+r.close()
+print(r)
